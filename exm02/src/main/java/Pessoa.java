dd23c3e68c67cc87e1d459f87bbea6cba2c552b6
Henrique Cardoso <henrique_acar@hotmail.com>
2022-05-17 19:08:30 -0300

    Cria Pessoa.java, PessoaRepository e modifica classe principal de startup

--- exm02/src/main/java/Pessoa.java
+++ exm02/src/main/java/Pessoa.java
@@ -1,9 +1,33 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
     private String nome;
     private Integer idade;
+
+    public Pessoa(){
+        this(null, null);
+    }
+
+    public Pessoa(String nome, Integer idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
     public String getNome() {
         return nome;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
     }
     public void setNome(String nome) {
         this.nome = nome;
--- exm02/src/main/java/br/ufjf/dcc193/henriquecardoso/exm02/Exm02Application.java
+++ exm02/src/main/java/br/ufjf/dcc193/henriquecardoso/exm02/Exm02Application.java
@@ -2,12 +2,21 @@ package br.ufjf.dcc193.henriquecardoso.exm02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Exm02Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Exm02Application.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(Exm02Application.class, args);
+		
+		PessoaRepository rep = ctx.getBean(PessoaRepository.class);
+		rep.save(new Pessoa("Fulano", 23));
+		rep.save(new Pessoa("Beltrano", 30));
+		rep.save(new Pessoa("Ciclana", 19));
+		System.out.println(rep.findAll().toString());
+
+
 	}
 
 }
