4cb309a5b4b0a17170ab6e92eaa21c84a64573b0
Henrique Cardoso <henrique_acar@hotmail.com>
2022-05-17 18:24:38 -0300

    Adiciona formulario.jsp, resultado.jsp e modifica HomeController

--- exm02/src/main/java/HomeController.java
+++ exm02/src/main/java/HomeController.java
@@ -1,11 +1,27 @@
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-    @RequestMapping
+    @RequestMapping({"", "index.html"})
     public String home(){
         return "home";
     }
+
+    @RequestMapping("formulario.html")
+    public String formulario(){
+        return "formulario";
+    }
+    @RequestMapping("resultado.html")
+    public ModelAndView resultado(String nome, Integer idade){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("resultado");
+        mv.addObject("nome", nome);
+        mv.addObject("idade", idade);
+        return mv;
+    }
+
+
     
 }
